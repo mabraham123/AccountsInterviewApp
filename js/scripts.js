a92e1b90fda2f8bebb9bc0f8381237829658d62f
mabraham123 <33029013+mabraham123@users.noreply.github.com>
2020-08-05 19:50:38 +0100

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var DeviceName="name";
+var DeviceCounter=0;
+
+function addDevice(){
+DeviceCounter+=1;
+var name= DeviceName+DeviceCounter;
+  var p_data='<li class="nav-item" role="presentation"><a class="nav-link" id="pills-'+name+'-tab" data-toggle="pill" href="#pills-'+name+'" role="tab" aria-controls="pills-'+name+'" aria-selected="false">'+name+'</a></li>';
+  document.getElementById("pills-tab").innerHTML += p_data;
+
+
+  var a_data='<div class="tab-pane fade" id="pills-'+name+'" role="tabpanel" aria-labelledby="pills-'+name+'-tab">vbvbvbvbsdads</div>';
+  document.getElementById("pills-tabContent").innerHTML += a_data;
+
+}
