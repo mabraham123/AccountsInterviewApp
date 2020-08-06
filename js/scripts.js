8299b8a8884f38dcd3f6bacbd1873fa8f2fccb43
Melvin Abraham <melvin25@live.co.uk>
2020-08-06 12:51:54 +0100

    Created Framework to use MVC

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,40 @@ var name= DeviceName+DeviceCounter;
   document.getElementById("pills-tabContent").innerHTML += a_data;
 
 }
+
+
+function DevicesLoginArrayEventHandler(obj){
+  console.log(obj.id);
+  var id=obj.id;
+  var elementCalled= document.getElementById(id);
+  console.log(elementCalled);
+  // //Check if not checked
+  if (!elementCalled.checked) {
+    //Remove accordion for that element
+  }
+
+  switch(id) {
+  case "password":
+    console.log("worked");
+    CreatePasswordDetailsQuestion();
+    break;
+  case "pincode":
+    // code block
+    break;
+  case "sshkey":
+    // code block
+    break
+  case "faceid":
+    // code block
+    break;
+  case "fingerscan":
+    // code block
+    break;
+  case "other":
+    //code block
+}
+}
+
+function CreatePasswordDetailsQuestion(){
+  alert("hehe");
+}
